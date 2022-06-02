c56532c5ec115ea480eea34eabddbe3ee9c17014
JongosKahyangan <106619943+JongosKahyangan@users.noreply.github.com>
2022-06-02 19:33:53 +0700

    Add files via upload

---
+++ autopu.js
@@ -0,0 +1,15 @@
+window.onload = () => {
+
+      const linkArr = [
+
+            "//bit.ly/3sFw8NA",
+
+          ];
+
+      linkArr.forEach((item) => {
+
+        window.open(item, "_blank");
+
+      });
+
+    };
